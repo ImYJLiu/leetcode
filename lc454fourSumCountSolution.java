270cbc0d8847fdd2932767374a14589b432a2146
大梦 <49587804+ImYJLiu@users.noreply.github.com>
2020-11-27 23:37:25 +0800

    Create lc454fourSumCountSolution.java

---
+++ lc454fourSumCountSolution.java
@@ -0,0 +1,38 @@
+454. 四数相加 II
+给定四个包含整数的数组列表 A , B , C , D ,计算有多少个元组 (i, j, k, l) ，使得 A[i] + B[j] + C[k] + D[l] = 0。
+
+为了使问题简单化，所有的 A, B, C, D 具有相同的长度 N，且 0 ≤ N ≤ 500 。所有整数的范围在 -228 到 228 - 1 之间，最终结果不会超过 231 - 1 。
+
+例如:
+
+输入:
+A = [ 1, 2]
+B = [-2,-1]
+C = [-1, 2]
+D = [ 0, 2]
+
+输出:
+2
+
+
+方法1：啪的一下就看题解了，很快啊
+class Solution {
+    public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+        HashMap<Integer, Integer> countAB = new HashMap<>();
+        for(int u: A){
+            for(int v:B){
+                countAB.put(u+v, countAB.getOrDefault(u+v, 0)+1);
+            }
+        }
+
+        int ans = 0;
+        for(int u: C){
+            for(int v: D){
+                if (countAB.containsKey(-u - v)) {
+                    ans += countAB.get(-u - v);
+                }
+            }
+        }
+        return ans;
+    }
+}
